dc908ed9b737570f9a1c2df57b8910450fe0c651
Dobromir Stefanov <dobromirstefanov96@gmail.com>
2018-09-09 16:44:51 +0300

    Added constructor, destructor and validation methods in Bank Account class

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <list>
+#include <cstdlib>
 using namespace std;
 
 /**
@@ -19,9 +20,47 @@ class BankAccount {
 		list<double> income;
 		list<double> outcome;
 
+	void create_unique_code() {
+		this->unique_code[0] = 65 + rand() % 25; // Get unique letter from A to Z;
+
+		// Set unique digit from 0 to 9
+		for (char i = 0; i < 4; i++) {
+			this->unique_code[i] = rand() % 9;
+		}
+	}
+
+	void set_holder_name(char *holder_name) {
+		if (sizeof(holder_name)/sizeof(char) < 5) {
+			cout << "Holder name must be at least 5 symbols. Insert new data." << endl;
+			delete this;
+			return;
+		}
+
+		this->holder_name = holder_name;
+	}
+
 	public:
 		// Initialize empty construct
 		BankAccount() {
-			// TODO ...
+			cout << "Created new account wih empty data." << endl;
+		}
+
+		BankAccount(char *unique_code, char *holder_name) {
+			create_unique_code();
+			set_holder_name(holder_name);
+
+			cout << "Holder: " << this->holder_name << " has new Bank Account with ID: " << this->unique_code << endl;
+		}
+
+		~BankAccount() {
+			cout << "Current account is destroyed!" << endl;
+		}
+
+		char *get_holder_name() {
+			return this->holder_name;
+		}
+
+		char *get_unique_code() {
+			return this->unique_code;
 		}
 };
