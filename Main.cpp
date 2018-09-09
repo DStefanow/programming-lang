7307ad11c2e8716d2bb575eaf7902254231280b7
Dobromir Stefanov <dobromirstefanov96@gmail.com>
2018-09-09 18:23:11 +0300

    Added test data in Main file

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,24 @@
 #include<iostream>
+#include <fstream>
 #include "core.h"
 
+void create_unique_code();
+
 int main() {
-	BankAccount acc;
+
+	char *name = "Dobromir Stefanov";
+	BankAccount acc(name);
+
+	ofstream out_file("accounts.txt");
+	out_file<<acc;
+	out_file.close();
+
+	BankAccount acc1;
+	ifstream in_file("accounts.txt");
+	in_file>>acc1;
+	in_file.close();
+
+	cout << "Account ID: " << acc1.get_unique_code() << endl;
 
 	return 0;
 }
