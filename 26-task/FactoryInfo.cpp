69aacfffc4d463de56ab421841e766716caa5f41
Dobromir Stefanov <dobromirstefanov96@gmail.com>
2018-09-19 22:54:03 +0300

    Created FactoryInfo class

---
+++ 26-task/FactoryInfo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#define ACC_FILE "accounts.txt"
+
+using namespace std;
+
+class FactoryInfo {
+	private:
+		char* country;
+		double price;
+
+	public:
+		FactoryInfo() {
+			cout << "Created empty object from class FactoryInfo" << endl;
+		}
+
+		FactoryInfo(char* country, double price) {
+			this->country = country;
+			this->price = price;
+		}
+
+		~FactoryInfo() {}
+};
